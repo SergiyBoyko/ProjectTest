96ffef1c8f6a1c8c8dccbb7dc711800db064b0f7
SergiyBoyko <developer.sergiyboyko@gmail.com>
2018-02-07 21:25:20 +0200

    Added fragments to main activity

--- app/src/main/java/com/example/serhii/ubrainianstest/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/serhii/ubrainianstest/ui/activities/MainActivity.java
@@ -1,9 +1,20 @@
 package com.example.serhii.ubrainianstest.ui.activities;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.serhii.ubrainianstest.R;
+import com.example.serhii.ubrainianstest.ui.fragments.GeoNamesFragment;
+import com.example.serhii.ubrainianstest.ui.fragments.PanoramaImageFragment;
+import com.example.serhii.ubrainianstest.ui.fragments.ZipImageFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +22,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        // Set Tabs inside Toolbar
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        Adapter adapter = new Adapter(getSupportFragmentManager());
+
+        adapter.addFragment(GeoNamesFragment.newInstance(), getResources().getString(R.string.geo_names_title));
+        adapter.addFragment(PanoramaImageFragment.newInstance(), getResources().getString(R.string.panorama_title));
+        adapter.addFragment(ZipImageFragment.newInstance(), getResources().getString(R.string.zip_image_title));
+
+        viewPager.setAdapter(adapter);
+    }
+
+    private class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public Adapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 }
