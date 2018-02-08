7f8ebe9e8b710113c286834af758391c69dfb4f8
SergiyBoyko <developer.sergiyboyko@gmail.com>
2018-02-08 14:22:57 +0200

    Implemented panorama image viewer

--- app/src/main/java/com/example/serhii/ubrainianstest/ui/fragments/PanoramaImageFragment.java
+++ app/src/main/java/com/example/serhii/ubrainianstest/ui/fragments/PanoramaImageFragment.java
@@ -1,6 +1,5 @@
 package com.example.serhii.ubrainianstest.ui.fragments;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -8,12 +7,17 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.serhii.ubrainianstest.R;
+import com.example.serhii.ubrainianstest.widgets.listeners.GyroscopeObserver;
+import com.example.serhii.ubrainianstest.widgets.views.PanoramaImageView;
 
 /**
  * Created by Serhii on 07.02.2018.
  */
 
 public class PanoramaImageFragment extends android.support.v4.app.Fragment {
+
+    private GyroscopeObserver gyroscopeObserver;
+
     public static PanoramaImageFragment newInstance() {
         return new PanoramaImageFragment();
     }
@@ -21,6 +25,27 @@ public class PanoramaImageFragment extends android.support.v4.app.Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_panorama, container, false);
+        View view = inflater.inflate(R.layout.fragment_panorama, container, false);
+
+        gyroscopeObserver = new GyroscopeObserver();
+
+        PanoramaImageView panoramaImageView = (PanoramaImageView) view.findViewById(R.id.panorama_image_view);
+        panoramaImageView.setGyroscopeObserver(gyroscopeObserver);
+
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Register GyroscopeObserver.
+        gyroscopeObserver.register(getActivity());
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // Unregister GyroscopeObserver.
+        gyroscopeObserver.unregister();
     }
 }
