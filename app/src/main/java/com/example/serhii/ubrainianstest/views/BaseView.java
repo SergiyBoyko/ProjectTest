c87427b1c04692e0131bc7c96060685cf0fedb5f
SergiyBoyko <developer.sergiyboyko@gmail.com>
2018-02-08 09:06:50 +0200

    Added Views

---
+++ app/src/main/java/com/example/serhii/ubrainianstest/views/BaseView.java
@@ -0,0 +1,12 @@
+package com.example.serhii.ubrainianstest.views;
+
+import android.content.Context;
+
+/**
+ * Created by Serhii on 08.02.2018.
+ */
+
+public interface BaseView {
+
+    Context getContext();
+}
